9df9a79998acc986131f06335e73a645d0610027
Vouther <lu_neji_shakugan09@hotmail.com>
2024-07-17 12:49:16 -0300

    Instalacion de Express y organizaion de carpetas

---
+++ BackEnd/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const methodOverride =  require('method-override'); // Pasar poder usar los métodos PUT y DELETE
+
+const applicantsRoutes = require('./src/routes/applicantsRoutes');
+const professionsRoutes = require('./src/routes/professionsRoutes');
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
+
+app.use(applicantsRoutes);
+app.use(professionsRoutes);
+
+app.listen('3001', () => console.log('Servidor corriendo en el puerto 3001'));
