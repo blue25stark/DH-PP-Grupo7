ec820a9048eedb55404286216fd1497fb06b86a9
Vouther <lu_neji_shakugan09@hotmail.com>
2024-07-20 19:21:10 -0300

    Configuraciones Tailwind

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,8 @@
-//import { useState } from 'react'
 import './App.css'
 import { Route,Routes } from 'react-router-dom'
 import PanelGeneral from './components/PanelGeneral'
 
 function App() {
-  //const [count, setCount] = useState(0)
 
   return (
     <>
